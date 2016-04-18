7a97f429e3a6c97a6de6b5a82321a13f3c72deb2
DavidJarl <kylochfrys@hotmail.com>
2016-04-18 14:48:24 +0200

    Uppgift 3 klar (?)

---
+++ Labb1Uppgift3OchFrammat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Labb1Uppgift3OchFrammat.MvcApplication" Language="C#" %>
---
+++ Labb1Uppgift3OchFrammat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Labb1Uppgift3OchFrammat.Startup))]
+namespace Labb1Uppgift3OchFrammat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
