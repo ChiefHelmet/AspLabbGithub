458a6075bd2aa433c21d07c8f0cac16bc6da1be1
DavidJarl <kylochfrys@hotmail.com>
2016-04-18 15:33:59 +0200

    Labb 1 uppgift 4 klar

--- Labb1Uppgift3OchFrammat/App_Start/RouteConfig.cs
+++ Labb1Uppgift3OchFrammat/App_Start/RouteConfig.cs
@@ -11,12 +11,22 @@ namespace Labb1Uppgift3OchFrammat
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
+            routes.MapRoute(
+                 name: "Sell",
+                 url: "Sell/{action}/{id}",
+                 defaults: new { controller = "Sell",
+                     action = "Completion",
+                     id = UrlParameter.Optional }
+);
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional }
             );
         }
     }
