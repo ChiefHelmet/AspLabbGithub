d3860f1e259cd7f6898a6908a333e0492d167154
DavidJarl <kylochfrys@hotmail.com>
2016-04-28 15:27:01 +0200

    Labb21 fungerar men har ingen routing

---
+++ Lab21/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class GameController : Controller
+    {
+        Models.Game model = new Models.Game();
+
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(string player, string number, string oldNum)
+        {
+            model.number = int.Parse(oldNum);
+            model.SetNumber(int.Parse(number));
+            return View(model);
+        }
+    }
+}
---
+++ Lab21/Models/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class Game
+    {
+        public int number;
+        public string result;
+        public void SetNumber(int num)
+        {
+            result = "";
+
+            //om nummer är 0 = datorn börjar spela
+            if (num == 0)
+            {
+                number = Random();
+                result = number.ToString();
+            }
+            else if (num == 1 || num == 2)
+            {
+                //Spelaren sätter ett värde
+                number += num;
+
+                //Kolla om spelaren är vinnare
+                if (number == 21 || number > 21)
+                {
+                    //Avsluta spelet
+                    result = "Spelaren är vinnare";
+                }
+                else
+                {
+                    //Data skall spela
+                    number += Random();
+
+                    //Kolla om datan är vinnare
+                    if (number == 21 | number > 21)
+                    {
+                        //Avsluta
+                        result = "Datan är vinnare";
+                    }
+                    else
+                    {
+                        result = number.ToString();
+                    }
+                }
+            }
+
+        }
+        private int Random()
+        {
+            return new Random().Next(1, 3);
+        }
+    }
+}
