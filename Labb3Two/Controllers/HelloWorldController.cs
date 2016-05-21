f32c279a494d1fe6cae9919faef0f22e27aa44f9
DavidJarl <kylochfrys@hotmail.com>
2016-05-21 20:21:49 +0200

    Adding A Controller Done

---
+++ Labb3Two/Controllers/HelloWorldController.cs
@@ -0,0 +1,52 @@
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Labb3Two.Controllers
+//{
+//    public class HelloWorldController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Labb3Two.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        
+
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+
+        //    return HtmlEncoder.Default.Encode("Hello, " + name + ", NumTimes is: " + numTimes);
+        //}
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode(
+        //        "Hello " + name + ", ID: " + ID);
+        //}
+
+
+    }
+}
---
+++ Labb3Two/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labb3Two.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
